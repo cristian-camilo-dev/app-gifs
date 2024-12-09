e23f20b10725621f71caf1b67d56a27d740887f0
cristian-camilo-dev <cristianrodrig149@gmail.com>
2024-12-09 17:25:02 +0100

    feat(desingGifs) 🎉 add card list component and integrate with search box

---
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  standalone: false,
+  templateUrl: './card-list.component.html',
+  styleUrl: './card-list.component.css',
+})
+export class CardListComponent {}
--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-box',
   standalone: false,
-  
+
   templateUrl: './search-box.component.html',
-  styleUrl: './search-box.component.css'
+  styleUrl: './search-box.component.css',
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor() {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log(newTag);
+  }
 }
