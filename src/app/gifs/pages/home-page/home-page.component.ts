47987b00e9158362f2e7c50ed1a3c5b30d87bf6f
cristian-camilo-dev <cristianrodrig149@gmail.com>
2024-12-24 11:33:20 +0100

    feat(gifsServices) remove reference to CSS file in home page component

--- src/app/gifs/pages/home-page/home-page.component.ts
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -5,9 +5,7 @@ import { GifData } from '../../interfaces/gifs.interfaces';
 @Component({
   selector: 'app-home-page',
   standalone: false,
-
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
   constructor(private gifsService: GifsService) {}
